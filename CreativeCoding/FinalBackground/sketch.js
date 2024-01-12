f36a78f64a6a2b84f59c1351d432869c5ea6939b
hojo17 <hojo1785@gmail.com>
2024-01-12 12:30:47 -0500

    BBEdit

--- CreativeCoding/FinalBackground/sketch.js
+++ CreativeCoding/FinalBackground/sketch.js
@@ -1,34 +1,34 @@
+	let gif;
+	
+	function preload(){
+	gif = loadImage ('assets/Circle.gif')
+	}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('blue');
-  strokeWeight(1);
   
-  fill('magenta');
 }
 
 function draw() {
 
-  var num = 50
-  var sidelen = windowWidth/num
+ background('green');
+
+  var num = 50;
+  var sidelen = windowWidth/num;
   
   for(var y = 0; y < windowHeight; y = y + sidelen)
     {
     
   for(var x = 0; x < windowWidth; x = x + sidelen)
   {
-    quad (
-    x,y,
-    x + sidelen,y,
-    x + sidelen,sidelen + y,
-    x,sidelen + y 
-    )
-  }
+  image (gif, x, y, sidelen, sideLen)
+  
     }
- 
+ }
 }
 
 
-function windowResize(){
-  resizeCanvas(windowWidth, windowHeight);
+function windowResized(){
+  resizeCanvas (windowWidth, windowHeight);
 }
 
