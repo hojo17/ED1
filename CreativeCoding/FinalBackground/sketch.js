4ef2cf94bfe415a233f8c705ad2b76d683eb45fe
hojo17 <hojo1785@gmail.com>
2024-01-29 10:09:41 -0500

    Update sketch.js

--- CreativeCoding/FinalBackground/sketch.js
+++ CreativeCoding/FinalBackground/sketch.js
@@ -19,12 +19,12 @@ background ('255,255,255    ');
  background('green');
 
   var num = 50
-  var sidelen = windowWidth/num;
+  var sidelen = windowWidth;
   
-  for(var y = 0; y < windowHeight; y = y + sidelen)
+  for(var y = 0; y < 2 * windowHeight; y = y + sidelen)
     {
     
-  for(var x = 0; x < windowWidth; x = x + sidelen)
+  for(var x = 0; x < 2 * windowWidth; x = x + sidelen)
   {
   image (gif, x, y, sidelen, sideLen)
   
